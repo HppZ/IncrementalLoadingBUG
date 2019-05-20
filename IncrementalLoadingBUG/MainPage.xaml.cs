afdce8d3de762b60d67d4dc298a964b80f491db1
haopeng <haopeng__@outlook.com>
2019-05-20 15:57:14 +0800

    init

---
+++ IncrementalLoadingBUG/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DataBinding;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IncrementalLoadingBUG
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ListViewElement.ItemsSource = new List<GeneratorIncrementalLoadingClass<string>>()
+            {
+                new GeneratorIncrementalLoadingClass<string>(100, i => { return i.ToString(); }),
+            };
+        }
+    }
+}
